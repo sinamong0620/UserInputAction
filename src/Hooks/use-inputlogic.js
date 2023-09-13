7c9faa37c940768dac144178b0650f9177056101
sinamong0620 <sinaskim60@gmail.com>
2023-09-14 02:17:22 +0900

    userForm useReducer Add

--- src/Hooks/use-inputlogic.js
+++ src/Hooks/use-inputlogic.js
@@ -1,26 +1,46 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
+
+const initialInputState = {
+  value: "",
+  isTouched: false,
+};
+const inputStateReducer = (state, action) => {
+  if (action.type === "INPUT") {
+    return { value: action.value, isTouched: state.isTouched };
+  }
+  if (action.type === "BLUR") {
+    return { isTouched: true, value: state.value };
+  }
+  if (action.type === "RESET") {
+    return { isTouched: false, value: "" };
+  }
+  return inputStateReducer;
+};
 const useInputLogic = (validFunc) => {
-  const [value, setValue] = useState("");
-  const [blurValue, setBlurValue] = useState(false);
-  const valueIsValid = validFunc(value);
-  const valudIsInValid = !valueIsValid && blurValue;
+  const [inputState, dispatch] = useReducer(
+    inputStateReducer,
+    initialInputState
+  );
+
+  const valueIsValid = validFunc(inputState.value);
+  const valudIsInValid = !valueIsValid && inputState.isTouched;
 
   const valueChangeHandler = ({ target }) => {
-    setValue(target.value);
+    dispatch({ type: "INPUT", value: target.value });
   };
 
   const valueBlurHandler = () => {
-    setBlurValue(true);
+    dispatch({ type: "BLUR" });
+
     //true로만 하고 다시 false로 돌리지 않아도 되는 걸까나?
   };
 
   const reset = () => {
-    setValue("");
-    setBlurValue(false);
+    dispatch({ type: "RESET" });
   };
 
   return {
-    value,
+    value: inputState.value,
     valueIsValid,
     valueChangeHandler,
     valueBlurHandler,
